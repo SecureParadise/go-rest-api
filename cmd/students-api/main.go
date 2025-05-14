3024778e6f83d13e7d3e189c0eb97f3f68906b90
SecureParadise <wrcbei078@gmail.com>
2025-05-14 23:28:53 +0545

    graceful shutdown mechanism

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	// "fmt"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/SecureParadise/students-api/internal/config"
 )
@@ -17,14 +23,32 @@ func main() {
 	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Welcome to student api"))
 	})
+
 	// sertup server
 	server := http.Server{
 		Addr:    cfg.Addr,
 		Handler: router,
 	}
-	fmt.Println("server started")
-	err := server.ListenAndServe()
+	// fmt.Printf("server started http://%s", cfg.HTTPServer.Addr)
+	slog.Info("server started ", slog.String("URL", "http://"+cfg.HTTPServer.Addr))
+
+	// graceful shutdown
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatal("failed to start server")
+		}
+	}()
+	<-done
+	slog.Info("Shutting Down the server")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err := server.Shutdown(ctx)
 	if err != nil {
-		log.Fatal("failed to start server")
+		slog.Error("failed to shutdown the server", slog.String("error", err.Error()))
 	}
+	slog.Info("server shutdown successfully")
 }
